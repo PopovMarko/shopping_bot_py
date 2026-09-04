dfac17b6e43bf6a15a2477da1b514ed9f7e3ac57
Mark Popov <hawk691@gmail.com>
2026-09-04 12:46:36 +0300

    DB table structure changed

--- src/shopping_bot/db/models.py
+++ src/shopping_bot/db/models.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from decimal import Decimal
 
 from sqlalchemy import BigInteger, Enum, ForeignKey, String, false
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import relationship as sa_relationship
 
 from shopping_bot.core.records.utils import RequestStatus
 
@@ -13,17 +14,9 @@ class Base(DeclarativeBase):
     pass
 
 
-class UserModel(Base):
-    __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
-    name: Mapped[str] = mapped_column(String(50))
-    is_admin: Mapped[bool] = mapped_column(default=False, server_default=false())
-    shopping_status: Mapped[bool] = mapped_column(default=False, server_default=false())
-    active_message_id: Mapped[int] = mapped_column(BigInteger, nullable=True)
-    shopping_started_at: Mapped[datetime | None]
-    purchases: Mapped[list[PurchaseModel]] = relationship(back_populates="user")
-    requests: Mapped[list[RequestModel]] = relationship(back_populates="user")
+def relationship(*args, **kwargs):
+    kwargs.setdefault("lazy", "selectin")
+    return sa_relationship(*args, **kwargs)
 
 
 class ProductModel(Base):
@@ -41,6 +34,29 @@ class StoreModel(Base):
     address: Mapped[str]
 
 
+class UserModel(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    name: Mapped[str] = mapped_column(String(50))
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default=false())
+    shopping_status: Mapped[bool] = mapped_column(default=False, server_default=false())
+    active_message_id: Mapped[int] = mapped_column(BigInteger, nullable=True)
+    shopping_started_at: Mapped[datetime | None]
+
+    requests: Mapped[list[RequestModel]] = relationship(
+        back_populates="requested_by_user",
+        foreign_keys="RequestModel.requested_by_user_id",
+    )
+    purchases: Mapped[list[RequestModel]] = relationship(
+        back_populates="purchased_by_user",
+        foreign_keys="RequestModel.purchased_by_user_id",
+    )
+    receipts: Mapped[list[ReceiptModel]] = relationship(
+        "ReceiptModel", back_populates="user"
+    )
+
+
 class RequestModel(Base):
     __tablename__ = "requests"
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -48,11 +64,26 @@ class RequestModel(Base):
     requested_by_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     requested_quantity: Mapped[Decimal]
     requested_at: Mapped[datetime]
+    purchased_by_user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), nullable=True
+    )
+    receipt_id: Mapped[int | None] = mapped_column(
+        ForeignKey("receipts.id"), nullable=True
+    )
+    price: Mapped[Decimal | None]
+    quantity: Mapped[Decimal | None]
+    match_confidence: Mapped[Decimal | None]
     status: Mapped[RequestStatus] = mapped_column(
         Enum(RequestStatus, name="request_status_constraint"),
         default=RequestStatus.pending,
     )
-    user: Mapped[UserModel] = relationship(back_populates="requests", lazy="selectin")
+
+    requested_by_user: Mapped[UserModel] = relationship(
+        back_populates="requests", foreign_keys=[requested_by_user_id]
+    )
+    purchased_by_user: Mapped[UserModel] = relationship(
+        back_populates="purchases", foreign_keys=[purchased_by_user_id]
+    )
     product: Mapped[ProductModel] = relationship(lazy="selectin")
 
 
@@ -66,16 +97,6 @@ class ReceiptModel(Base):
     raw_model_response: Mapped[str | None]
     created_at: Mapped[datetime]
 
-
-class PurchaseModel(Base):
-    __tablename__ = "purchases"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    purchased_by_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    request_id: Mapped[int | None] = mapped_column(ForeignKey("requests.id"))
-    receipt_id: Mapped[int | None] = mapped_column(ForeignKey("receipts.id"))
-    store_id: Mapped[int] = mapped_column(ForeignKey("stores.id"))
-    price: Mapped[Decimal]
-    quantity: Mapped[Decimal]
-    purchased_at: Mapped[datetime]
-    match_confidence: Mapped[Decimal]
-    user: Mapped[UserModel] = relationship(back_populates="purchases")
+    user: Mapped[UserModel] = relationship(
+        UserModel, back_populates="receipts", lazy="selectin"
+    )
