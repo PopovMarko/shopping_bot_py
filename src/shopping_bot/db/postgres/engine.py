0173e58b0f4789b22c6b6d6022a94e5709667250
Mark Popov <hawk691@gmail.com>
2026-09-14 13:41:16 +0300

    engine.py

--- src/shopping_bot/db/postgres/engine.py
+++ src/shopping_bot/db/postgres/engine.py
@@ -1,4 +1,5 @@
 import os
+from functools import lru_cache
 
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import (
@@ -7,15 +8,15 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
+load_dotenv()
+
+
+@lru_cache(maxsize=1)
+def get_engine() -> AsyncEngine:
+    database_url = str(os.getenv("DATABASE_URL"))
+    return create_async_engine(database_url, echo=False)
+
 
 def async_session_factory():
-
-    load_dotenv()
-
-    DATABASE_URL = str(os.getenv("DATABASE_URL"))
-
-    engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=False)
-
-    async_session_ = async_sessionmaker(bind=engine, expire_on_commit=False)
-
-    return async_session_()
+    async_session_maker = async_sessionmaker(bind=get_engine(), expire_on_commit=False)
+    return async_session_maker()
